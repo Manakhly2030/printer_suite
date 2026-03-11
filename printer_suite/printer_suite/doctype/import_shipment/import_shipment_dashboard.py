bbff19874d1343011216a07c41027262e829c495
Mohamed Mostafa <146191698+Manakhly2030@users.noreply.github.com>
2026-03-11 02:28:59 +0200

    Add fieldname to get_data function in import_shipment_dashboard

--- printer_suite/printer_suite/doctype/import_shipment/import_shipment_dashboard.py
+++ printer_suite/printer_suite/doctype/import_shipment/import_shipment_dashboard.py
@@ -1,16 +1,16 @@
 from frappe import _
 
-
 def get_data():
     return {
+        "fieldname": "custom_import_shipment",
         "non_standard_fieldnames": {
             "Purchase Receipt": "custom_import_shipment",
-            "Landed Cost Voucher": "custom_import_shipment"
+            "Landed Cost Voucher": "custom_import_shipment",
         },
         "transactions": [
             {
                 "label": _("Procurement"),
-                "items": ["Purchase Receipt", "Landed Cost Voucher"]
+                "items": ["Purchase Receipt", "Landed Cost Voucher"],
             }
-        ]
+        ],
     }
